d00e530e9ae3f72faec0bb5dbaca403f53c1cb8a
TheoMly <119632664+TheoMly@users.noreply.github.com>
2023-11-21 09:49:43 +0100

    lectureFichier V3
    traitement des 2 fichiers en simultané

--- partieThéo/lectureFichier.c
+++ partieThéo/lectureFichier.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void lecture(int **tabS1, int **tabS2, int *nbLignes){
+void lecture(int **tabS1, int **tabS2, int *nbLignes, char *nomFichier){
     FILE *fichier = NULL;
     char ligne[10];
     int compteur=0;
 
-    fichier = fopen("arete.txt","r"); // ouverture du fichier
+    fichier = fopen(nomFichier,"r"); // ouverture du fichier
     if (fichier == NULL){
         printf("Erreur ouverture fichier");
         exit(1);
@@ -16,11 +16,11 @@ void lecture(int **tabS1, int **tabS2, int *nbLignes){
         (*nbLignes)++;
     }
 
-    // Allouer dynamiquement les tableaux en fonction de la taille
+    // allocation dynamique
     *tabS1 = (int*)malloc((*nbLignes)*sizeof(int));
     *tabS2 = (int*)malloc((*nbLignes)*sizeof(int));
 
-    // Retourner une erreur si l'allocation échoue
+    // si problème avec les tableaux on sort du programme
     if (*tabS1 ==NULL || *tabS2==NULL){
         printf("Erreur d'allocation de mémoire");
         fclose(fichier);
@@ -29,7 +29,7 @@ void lecture(int **tabS1, int **tabS2, int *nbLignes){
         exit(1);
     }
 
-    // Réinitialiser le pointeur du fichier au début
+    // réinitialiser le pointeur du fichier au début
     fseek(fichier, 0, SEEK_SET);
 
     // vérifier si la ligne n'est pas vide et si elle contient bien 2 entiers
@@ -39,24 +39,35 @@ void lecture(int **tabS1, int **tabS2, int *nbLignes){
         }
     }
 
-    // Fermer le fichier
     fclose(fichier);
 }
 
 int main(){
-    int *tabS1, *tabS2;
+    int *tabPrecedence1, *tabPrecedence2;
+    int *tabTempsCycle1, *tabTempsCycle2;
     int nbLignes = 0;
+    char nomFichier[20];
 
-    lecture(&tabS1, &tabS2, &nbLignes);
-
+    printf("Donnez le nom du fichier precedences:");
+    scanf("%s", nomFichier);
+    lecture(&tabPrecedence1, &tabPrecedence2, &nbLignes, nomFichier);
     for (int i=0; i<nbLignes; i++){            //vérifier les données
-        printf("%d %d\n", tabS1[i], tabS2[i]);
+        printf("%d %d\n", tabPrecedence1[i], tabPrecedence2[i]);
+    }
+    printf("\nNombre de lignes: %d\n", nbLignes);
+    nbLignes = 0;
+
+    printf("Donnez le nom du fichier temps de cycle:");
+    scanf("%s", nomFichier);
+    lecture(&tabTempsCycle1, &tabTempsCycle2, &nbLignes, nomFichier);
+    for (int i=0; i<nbLignes; i++){            //vérifier les données
+        printf("%d %d\n", tabPrecedence1[i], tabPrecedence2[i]);
     }
     printf("\nNombre de lignes: %d\n", nbLignes);
 
     //libérer la memoire
-    free(tabS1);
-    free(tabS2);
+    free(tabPrecedence1);
+    free(tabPrecedence2);
 
     return 0;
 }
