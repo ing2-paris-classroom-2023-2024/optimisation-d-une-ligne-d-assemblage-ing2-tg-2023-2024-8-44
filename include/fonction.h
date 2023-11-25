1904af422128c35f6862fce0fea006e65134c9b8
mathe2810 <matheogleon92100@yahoo.com>
2023-11-25 16:12:21 +0100

    reaload project

---
+++ include/fonction.h
@@ -0,0 +1,18 @@
+//
+// Created by mathe on 11/11/2023.
+//
+
+#ifndef OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_8_44_FONCTION_H
+#define OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_8_44_FONCTION_H
+
+#include "stdlib.h"
+#include "stdio.h"
+typedef struct {
+    int operation1;
+    int operation2;
+} t_Exclusion;
+
+void lecture(int **tabS1, int **tabS2, int *nbLignes, char *nomFichier);
+
+
+#endif //OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_8_44_FONCTION_H
