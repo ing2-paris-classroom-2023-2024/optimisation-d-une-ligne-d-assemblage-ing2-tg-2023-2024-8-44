803eb3694e0b4da89871b17c8851c623b3d1ddac
Tony-sopranos <131447528+Tony-sopranos@users.noreply.github.com>
2023-11-25 17:26:51 +0100

    contrainte exclusion lire fichier

---
+++ cmake-build-debug/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/main.c.obj"
+  "CMakeFiles/project.dir/main.c.obj.d"
+  "CMakeFiles/project.dir/src/fonction.c.obj"
+  "CMakeFiles/project.dir/src/fonction.c.obj.d"
+  "CMakeFiles/project.dir/src/test.c.obj"
+  "CMakeFiles/project.dir/src/test.c.obj.d"
+  "libproject.dll.a"
+  "project.exe"
+  "project.exe.manifest"
+  "project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
