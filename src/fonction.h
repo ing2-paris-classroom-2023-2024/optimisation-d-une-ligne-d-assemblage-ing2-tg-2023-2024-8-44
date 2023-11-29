0df6958daddb2db94ccfbef3a55f8865c0d1074c
mathe2810 <matheogleon92100@yahoo.com>
2023-11-29 16:32:59 +0100

    lecture de fichier et changement dans les fichiers .txt

--- src/fonction.h
+++ src/fonction.h
@@ -7,11 +7,63 @@
 
 #include "stdlib.h"
 #include "stdio.h"
+#include "stdbool.h"
+
 typedef struct {
     double tempsOperation;
     int *exclusion;
     int *precedence;
 } t_operation;
+
+typedef struct arc_s {
+    struct arc_s *next;
+    struct sommet_s *sommet;
+    struct sommet_s *origin;
+} arc_t;
+
+typedef struct sommet_s {
+    int id;
+    int deg;
+    short couleur;
+    int pred;
+    int dist;
+    arc_t *arc;
+} sommet_t;
+
+typedef struct graphe_s {
+    int taille;
+    int ordre;
+    sommet_t **sommets;
+} graphe_t;
+
+
+typedef struct ope_s
+{
+    int id;
+    double time;
+} ope_t;
+
+typedef struct maillon_s
+{
+    ope_t *ope;
+    struct maillon_s *next;
+} maillon_t;
+
+typedef struct workStation_s
+{
+    maillon_t *ope;
+    double time;
+} workStation_t;
+
+typedef struct assemblyLine_s
+{
+    ope_t **ope;
+    workStation_t **workStation;
+    graphe_t *graph;
+    bool **unassociable;
+    double cycleTime;
+    int nbOpe;
+} assemblyLine_t;
 
 typedef struct
 {
